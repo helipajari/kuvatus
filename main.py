5beda2c4c8954c64d59736c35dd18855b28b3a41
Heli Pajari <h.v.m.pajari@student.tudelft.nl>
2024-11-07 13:10:30 +0200

    refactor names from nameName to name_name

--- main.py
+++ main.py
@@ -11,23 +11,24 @@ version = 1.0
 
 
 # TODO make UI bigger
+# returns tuple with parameters
 def dialog():
     gui.theme('DarkAmber')
 
-    # on laptops try D because usually no extra stuff
-    initMem = os.path.join('D:\\', '')
-    mem = gui.FolderBrowse('Muuta', initial_folder=initMem, key='mem')
-    memT = "etsi kansio"
+    # on laptops try D because usually no extra discs
+    init_mem = os.path.join('D:\\', '')
+    mem = gui.FolderBrowse('Muuta', initial_folder=init_mem, key='mem')
+    mem_t = "etsi kansio"
 
-    initPic = os.path.join(os.environ['USERPROFILE'], 'pictures')
-    pic = gui.FolderBrowse('Muuta', initial_folder=initPic, key='pic')
-    picT = "etsi kansio"
+    init_pic = os.path.join(os.environ['USERPROFILE'], 'pictures')
+    pic = gui.FolderBrowse('Muuta', initial_folder=init_pic, key='pic')
+    pic_t = "etsi kansio"
 
     ok = gui.Button('Ok', disabled=False)
 
-    layout = [[gui.Text('Lähdekansion polku: '), gui.Text(memT), mem],
-              [gui.Text('Kohdekansion polku: '), gui.Text(initPic), pic],
-              [gui.Checkbox("Poista siirretyt kuvat alkuperäisestä kansiosta?")],
+    layout = [[gui.Text('Lähdekansion polku: '), gui.Text(mem_t), mem],
+              [gui.Text('Kohdekansion polku: '), gui.Text(init_pic), pic],
+              [gui.Checkbox("Poista siirretyt kuvat lähdekansiosta?")],
               [ok, gui.Button('Peruuta')]]
 
     # WINDOW MADE HERE
@@ -41,14 +42,14 @@ def dialog():
 
         if event == 'Ok':
             # TODO disable ok unless mem and pic are not default value?
-            if layout[0][1].get() == memT:
+            if layout[0][1].get() == mem_t:
                 continue
             # mem, pic, delete
             return layout[0][1].get(), layout[1][1].get(), layout[2][0].get()
     window.close()
 
 
-def doneDialog():
+def done_dialog():
     gui.theme('DarkAmber')
 
     layout = [[gui.Text('Valmis!')], [], [],
@@ -66,7 +67,7 @@ def doneDialog():
 
 # returns a set or array of file creation dates in the format yyyy-mm
 # if file or folder has a name fitting with the format or "yyyy mm", add that to set instead
-def findDates(src, files, makeset):
+def find_dates(src, files, make_set):
     dates = []
     for filename in files:
         # if name is in format yyyy-mm or yyyy mm, add to set if possible
@@ -90,7 +91,7 @@ def findDates(src, files, makeset):
             space = T_stamp.split(" ")[0].split("-")
             date = space[0] + "-" + space[1]
 
-        if makeset:
+        if make_set:
             if date not in dates:
                 dates.append(date)
         else:
@@ -99,17 +100,17 @@ def findDates(src, files, makeset):
     return dates
 
 
-# if rmv False, copies files from src to dst, else replaces
-def moveFiles(src, dst, rmv):
+# if rmv False, copy files from src to dst, else move
+def move_files(src, dst, rmv):
     # look up folders that already exist
-    existingFolders = next(os.walk(dst))[1]
+    existing_folders = next(os.walk(dst))[1]
 
     # print("existingFolders ", existingFolders)
 
     # look up folder dates in the existing folders
-    oldDates = findDates(dst, existingFolders, True)
+    old_dates = find_dates(dst, existing_folders, True)
 
-    newDates = []
+    new_dates = []
 
     # print('pre-existing folders ', oldDates)
 
@@ -117,46 +118,47 @@ def moveFiles(src, dst, rmv):
     files = [f for f in os.listdir(src) if os.path.isfile(os.path.join(src, f))]
 
     # keep a list of all creation dates
-    fileDates = findDates(src, files, False)
+    file_dates = find_dates(src, files, False)
 
     # filter
-    for date in fileDates:
-        if date not in oldDates and date not in newDates:
-            newDates.append(date)
+    for date in file_dates:
+        if date not in old_dates and date not in new_dates:
+            new_dates.append(date)
 
     # print("oldDates ", oldDates)
     # print("newDates ", newDates)
 
     # make new folders
-    for n in newDates:
+    for n in new_dates:
         # print(os.path.join(dst, n))
         os.makedirs(os.path.join(dst, n))
 
     # copy or move files
     for f in files:
-        sourceFile = os.path.join(src, f)
-        dstFile = os.path.join(dst, fileDates[files.index(f)], f)
+        source_file = os.path.join(src, f)
+        dst_file = os.path.join(dst, file_dates[files.index(f)], f)
 
         # remove
         if rmv:
-            os.rename(sourceFile, dstFile)
+            os.rename(source_file, dst_file)
             # print("file 'removed'")
         else:
             # TODO make folder if it doesn't exist
-            shutil.copy2(sourceFile, dstFile)
+            shutil.copy2(source_file, dst_file)
             # print("file 'copied'")
 
 
 if __name__ == '__main__':
     # get parameters from UI
-    paths = dialog()
+    params = dialog()
 
-    if paths is None:
+    if params is None:
         sys.exit()
 
-    thread = threading.Thread(moveFiles(paths[0], paths[1], paths[2]))
+    (src, dst, rmv) = (params[0], params[1], params[2])
+    thread = threading.Thread(move_files(src, dst, rmv))
     thread.start()
 
-    doneDialog()
+    done_dialog()
 
 
