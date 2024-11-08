eb79af87508da0ac07b43ff8d1d533d43dc6b0b1
Heli Pajari <h.v.m.pajari@student.tudelft.nl>
2024-11-08 11:42:26 +0200

    rewrote logic to move files

--- main.py
+++ main.py
@@ -65,87 +65,50 @@ def done_dialog():
     window.close()
 
 
-# returns a set or array of file creation dates in the format yyyy-mm
-# if file or folder has a name fitting with the format or "yyyy mm", add that to set instead
-def find_dates(src, files, make_set):
-    dates = []
-    for filename in files:
-        # if name is in format yyyy-mm or yyyy mm, add to set if possible
-        # format check
-        x = re.findall("[1-2][0-9]{3}[-, " "][0-1][0-9]", filename)
-
-        date = ""
-
-        if len(x) == 1:
-            # trim to first 6 characters (in case of folder names like "2000-01 birthday")
-            date = filename[:len('yyyy-mm'):]
-        else:
-            f = os.path.join(src, filename)
-
-            ctime = os.path.getctime(f)
-            t_obj = time.strptime(time.ctime(ctime))
-
-            # Transforming the time object to a timestamp
-            # of ISO 8601 format
-            T_stamp = time.strftime("%Y-%m-%d %H:%M:%S", t_obj)
-            space = T_stamp.split(" ")[0].split("-")
-            date = space[0] + "-" + space[1]
-
-        if make_set:
-            if date not in dates:
-                dates.append(date)
-        else:
-            dates.append(date)
-
-    return dates
+def path_exists(path_parts: [str]):
+    path = path_join(path_parts)
+    return os.path.exists(path)
 
 
-# if rmv False, copy files from src to dst, else move
+def path_join(path_parts: [str]):
+    return '/'.join(path_parts)
+
+
+# Copy files from src to dst if rmv True, else move
 def move_files(src, dst, rmv):
-    # look up folders that already exist
-    existing_folders = next(os.walk(dst))[1]
-
-    # print("existingFolders ", existingFolders)
-
-    # look up folder dates in the existing folders
-    old_dates = find_dates(dst, existing_folders, True)
-
-    new_dates = []
-
-    # print('pre-existing folders ', oldDates)
-
-    # look up files from source
     files = [f for f in os.listdir(src) if os.path.isfile(os.path.join(src, f))]
 
-    # keep a list of all creation dates
-    file_dates = find_dates(src, files, False)
+    for file in files:
+        year, month = file[:4], file[4:6]
 
-    # filter
-    for date in file_dates:
-        if date not in old_dates and date not in new_dates:
-            new_dates.append(date)
+        path = [dst, year]
 
-    # print("oldDates ", oldDates)
-    # print("newDates ", newDates)
+        if not path_exists(path):
+            os.mkdir(path_join(path))
+            print("made ", year)
 
-    # make new folders
-    for n in new_dates:
-        # print(os.path.join(dst, n))
-        os.makedirs(os.path.join(dst, n))
+        path.append(month)
 
-    # copy or move files
-    for f in files:
-        source_file = os.path.join(src, f)
-        dst_file = os.path.join(dst, file_dates[files.index(f)], f)
+        if not path_exists(path):
+            os.mkdir(path_join(path))
+            print("made ", month)
 
-        # remove
-        if rmv:
-            os.rename(source_file, dst_file)
-            # print("file 'removed'")
-        else:
-            # TODO make folder if it doesn't exist
-            shutil.copy2(source_file, dst_file)
-            # print("file 'copied'")
+        path.append(file)
+
+        file_source = path_join([src, file])
+        file_destination = path_join(path)
+
+        if not os.path.exists(file_destination):  # either move or copy
+            if rmv:
+                os.rename(file_source, file_destination)
+                print("moved")
+            else:
+                shutil.copy(file_source, file_destination)
+                print("copied")
+
+        elif rmv:  # remove or do nothing
+            os.remove(file_source)
+            print("deleted")
 
 
 if __name__ == '__main__':
@@ -155,8 +118,8 @@ if __name__ == '__main__':
     if params is None:
         sys.exit()
 
-    (src, dst, rmv) = (params[0], params[1], params[2])
-    thread = threading.Thread(move_files(src, dst, rmv))
+    (source, destination, remove) = (params[0], params[1], params[2])
+    thread = threading.Thread(move_files(source, destination, remove))
     thread.start()
 
     done_dialog()
